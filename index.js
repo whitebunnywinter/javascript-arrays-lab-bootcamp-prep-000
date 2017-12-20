f72cfba1d348a86c4bb1216fe72d749f6314a6cf
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-20 07:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var newArray =[...kittens]
+  newArray.push(name)
+  return newArray
+}
